3d5056a55088f155defe80a4d4237d74f220c6f9
kpinakula <7438240+kpinakula@users.noreply.github.com>
2025-01-29 10:45:50 +0000

    Implement equals method for volume

--- volume.js
+++ volume.js
@@ -5,17 +5,66 @@ export class Volume {
     }
 
     equals(other) {
-        return other.amount === this.amount && other.unit === this.unit
+        const thisAsTeaspoon = this.#asTeaspoon(this);
+        const otherAsTeaspoon = this.#asTeaspoon(other);
+
+        return thisAsTeaspoon.amount === otherAsTeaspoon.amount && thisAsTeaspoon.unit === otherAsTeaspoon.unit
     }
+
+    #asTeaspoon(volume) {
+        var amount;
+        const unit = Units.TEASPOON;
+
+        switch (volume.unit) {
+            case Units.TABLESPOON:
+                amount = volume.amount * 3;
+                break;
+            case Units.OUNCE:
+                amount = volume.amount * 3 * 2;
+                break;
+            case Units.CUP:
+                amount = volume.amount * 3 * 2 * 8;
+                break;
+            case Units.PINT:
+                amount = volume.amount * 3 * 2 * 8 * 2;
+                break;
+            case Units.QUART:
+                amount = volume.amount * 3 * 2 * 8 * 2 * 2;
+                break;
+            case Units.GALLON:
+                amount = volume.amount * 3 * 2 * 8 * 2 * 2 * 4;
+                break;
+            default:
+                amount = volume.amount;
+                break;
+        }
+
+        return new Volume(amount, unit);
+    }
+
+    // add(other){
+    //     const
+    //     // this.Volume = "2 teaspoons"
+
+    // }
 }
 
-export class Teaspoon {
+export class Units {
+    static TEASPOON = 'tsp';
+    static TABLESPOON = 'tbsp';
+    static OUNCE = 'oz';
+    static CUP = 'cup';
+    static PINT = 'pint';
+    static QUART = 'quart';
+    static GALLON = 'gallon';
 }
 
-export class Tablespoon {
-}
 
-// export class Units {
-//     static TEASPOON = 'tsp';
-//     static TABLESPOON = 'tbsp';
-// }
+// conversions to teaspoon
+// 1 tbsp = 3 tsp
+// 1 oz = 2 tbsp = 6 tsp
+// 1 cup = 8 oz = 16 tbsp = 48 tsp
+// 1 pint = 2 cups = 96 tsp
+// 1 quart = 2 pints = 192 tsp
+// 1 gallon = 4 quarts = 768 tsp
+
--- volume.test.js
+++ volume.test.js
@@ -1,26 +1,52 @@
-import { Teaspoon, Tablespoon, Volume } from './volume';
+import { Units, Volume } from "./volume";
 
-describe('Volume', () => {
-  it('two volumes of the same type and amount should equal one another', () => {
-    const teaspoon = new Teaspoon();
-    const volume1 = new Volume(1, teaspoon);
-    const volume2 = new Volume(1, teaspoon);
+describe("Volume", () => {
+  describe.each([
+    { unit: Units.TEASPOON, amount: 1 },
+    { unit: Units.TABLESPOON, amount: 3 },
+    { unit: Units.OUNCE, amount: 6 },
+    { unit: Units.CUP, amount: 48 },
+    { unit: Units.PINT, amount: 96 },
+    { unit: Units.QUART, amount: 192 },
+    { unit: Units.GALLON, amount: 768 }
+  ])(
+    "one volume is equivalent to its corresponding  volume in teaspoons",
+    ({ unit, amount }) => {
+      test(`amount: ${amount} & unit: ${unit}`, () => {
+        const volume1 = new Volume(amount, Units.TEASPOON);
+        const volume2 = new Volume(1, unit);
 
-    expect(volume1.equals(volume2)).toBe(true);
-  });
-  it('two volumes of the same type and different amount should not equal one another', () => {
-    const teaspoon = new Teaspoon();
-    const volume1 = new Volume(1, teaspoon);
-    const volume2 = new Volume(2, teaspoon);
+        expect(volume1.equals(volume2)).toBe(true);
+      });
+    }
+  );
+  it("2 teaspoons is not equivalent to 3 teaspoons", () => {
+    const volume1 = new Volume(2, Units.TEASPOON);
+    const volume2 = new Volume(3, Units.TEASPOON);
 
     expect(volume1.equals(volume2)).toBe(false);
   });
-  it('two volumes of a different type and the same amount should not equal one another', () => {
-    const teaspoon = new Teaspoon();
-    const tablespoon = new Tablespoon();
+  it("1 teaspoon is not equivalent to 1 tablespoon", () => {
+    const volume1 = new Volume(1, Units.TEASPOON);
+    const volume2 = new Volume(1, Units.TABLESPOON);
 
-    const volume1 = new Volume(1, teaspoon);
-    const volume2 = new Volume(1, tablespoon);
+    expect(volume1.equals(volume2)).toBe(false);
+  });
+  it("1 tablespoon is equivalent to 3 teaspoons", () => {
+    const volume1 = new Volume(1, Units.TABLESPOON);
+    const volume2 = new Volume(3, Units.TEASPOON);
+
+    expect(volume1.equals(volume2)).toBe(true);
+  });
+  it("3 teaspoons are equivalent to 1 tablespoon", () => {
+    const volume1 = new Volume(3, Units.TEASPOON);
+    const volume2 = new Volume(1, Units.TABLESPOON);
+
+    expect(volume1.equals(volume2)).toBe(true);
+  });
+  it("two volumes of different type and different amount and not equivalent should not equal one another", () => {
+    const volume1 = new Volume(3, Units.TEASPOON);
+    const volume2 = new Volume(2, Units.TABLESPOON);
 
     expect(volume1.equals(volume2)).toBe(false);
   });
