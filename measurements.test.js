619d0cd69fbbd47e18338325d60137cf909a827b
kpinakula <7438240+kpinakula@users.noreply.github.com>
2025-01-29 18:24:58 +0000

    Add test cases for increased test coverage & confidence

--- measurements.test.js
+++ measurements.test.js
@@ -16,12 +16,6 @@ describe('Test volume...', () => {
 
             expect(volume1.equals(volume2)).toBe(true);
         })
-        test('two measurements with incompatible units are not equivalent', () => {
-            const volume1 = new Measurement(1, Units.VOLUME.TEASPOON);
-            const length1 = new Measurement(1, Units.LENGTH.INCH);
-
-            expect(volume1.equals(length1)).toBe(false);
-        })
     });
 
     describe('addition...', () => {
@@ -45,13 +39,26 @@ describe('Test volume...', () => {
     });
 });
 
-
-
-
-
 describe('Test length...', () => {
     describe('equality...', () => {
+        test('two lengths of same unit and quantity are equivalent', () => {
+            const length1 = new Measurement(1, Units.LENGTH.INCH);
+            const length2 = new Measurement(1, Units.LENGTH.INCH);
 
+            expect(length1.equals(length2)).toBe(true);
+        })
+        test('two lengths with equivalent quantity in different units are equivalent', () => {
+            const volume1 = new Measurement(1, Units.LENGTH.FOOT);
+            const volume2 = new Measurement(12, Units.LENGTH.INCH);
+
+            expect(volume1.equals(volume2)).toBe(true);
+        })
+        test('two measurements with incompatible units are not equivalent', () => {
+            const volume1 = new Measurement(1, Units.VOLUME.TEASPOON);
+            const length1 = new Measurement(1, Units.LENGTH.INCH);
+
+            expect(volume1.equals(length1)).toBe(false);
+        })
     });
     describe('addition...', () => {
         test('12 inches plus 1 foot is equivalent to 2 feet', () => {
@@ -97,6 +104,27 @@ describe('Test temperature...', () => {
         test('0 Celsius is equivalent to 32 Fahrenheit', () => {
             const temperature1 = new Measurement(32, new TemperatureUnit("Fahrenheit"));
             const temperature2 = new Measurement(0, new TemperatureUnit("Celcius"));
+
+            const actual = temperature1.equals(temperature2);
+            expect(actual).toBe(true);
+        })
+        test('100 Celsius is equivalent to 212 Fahrenheit', () => {
+            const temperature1 = new Measurement(100, new TemperatureUnit("Celcius"));
+            const temperature2 = new Measurement(212, new TemperatureUnit("Fahrenheit"));
+
+            const actual = temperature1.equals(temperature2);
+            expect(actual).toBe(true);
+        })
+        test('10 Celsius is equivalent to 50 Fahrenheit', () => {
+            const temperature1 = new Measurement(-40, new TemperatureUnit("Celcius"));
+            const temperature2 = new Measurement(-40, new TemperatureUnit("Fahrenheit"));
+
+            const actual = temperature1.equals(temperature2);
+            expect(actual).toBe(true);
+        })
+        test('-40 Celsius is equivalent to -40 Fahrenheit', () => {
+            const temperature1 = new Measurement(-40, new TemperatureUnit("Celcius"));
+            const temperature2 = new Measurement(-40, new TemperatureUnit("Fahrenheit"));
 
             const actual = temperature1.equals(temperature2);
             expect(actual).toBe(true);
