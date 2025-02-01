c7f503318c00cbeca9486a50cd5584cce5453e7e
kpinakula <7438240+kpinakula@users.noreply.github.com>
2025-02-01 19:06:18 +0000

    Add length conversions for yd, fur and mi
    This commit also adds a standard unit to the LengthUnit and the
    TemperatureUnit constructors.

--- unitsOfMeasurements.js
+++ unitsOfMeasurements.js
@@ -43,33 +43,52 @@ export class VolumeUnit {
 export class LengthUnit {
     constructor(unit) {
         this.unit = unit;
+        this.standardUnit = LengthUnit.INCH;
     }
 
-    static INCH = 'inch';
-    static FOOT = 'foot';
+    static INCH = 'in';
+    static FOOT = 'ft';
+    static YARD = 'yd';
+    static FURLONG = 'fur';
+    static MILE = 'mi';
+
+    conversionTable = {
+        [LengthUnit.INCH]: { multiplier: 1, targetUnit: LengthUnit.INCH },
+        [LengthUnit.FOOT]: { multiplier: 12, targetUnit: LengthUnit.INCH },
+        [LengthUnit.YARD]: { multiplier: 3, targetUnit: LengthUnit.FOOT },
+        [LengthUnit.FURLONG]: { multiplier: 220, targetUnit: LengthUnit.YARD },
+        [LengthUnit.MILE]: { multiplier: 8, targetUnit: LengthUnit.FURLONG },
+    };
+
+    #convert(unit, quantity) {
+        const {multiplier, targetUnit} = this.conversionTable[unit];
+        const convertedQuantity = multiplier * quantity;
+
+        return targetUnit === this.standardUnit
+            ? convertedQuantity
+            : this.#convert(targetUnit, convertedQuantity)
+    }
 
     toStandard() {
-        return new LengthUnit(LengthUnit.INCH);
+        return new LengthUnit(this.standardUnit);
     }
 
     toStandardQuantity(quantity) {
-        if (this.unit === LengthUnit.FOOT) {
-            return quantity * 12;
-        }
-        return quantity;
+        return this.#convert(this.unit, quantity);
     }
 }
 
 export class TemperatureUnit {
     constructor(unit) {
         this.unit = unit;
+        this.standardUnit = TemperatureUnit.CELSIUS;
     }
 
-    static CELSIUS = 'celsius';
-    static FAHRENHEIT = 'fahrenheit';
+    static CELSIUS = 'C';
+    static FAHRENHEIT = 'F';
 
     toStandard() {
-        return new TemperatureUnit(TemperatureUnit.CELSIUS);
+        return new TemperatureUnit(this.standardUnit);
     }
 
     toStandardQuantity(degree) {
